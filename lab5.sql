f84a72dddb602178cbd1f219285251f97a676142
Sahra Zhou <63219465+Sahra-Zhou@users.noreply.github.com>
2022-05-19 22:33:34 -0700

    Add files via upload

---
+++ lab5.sql
@@ -0,0 +1,152 @@
+-- lab5
+
+-- possible responses
+-- insert into Possibilities (qtype, QuestionNo, ResponseID, TextPrompt) values();
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 1, "1 - strongly disagree ");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 2, "2- disagree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 3, "3 - somewhat disagree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 4, "4 - neither agree nor disagree ");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 5, "5 - somewhat agree ");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 6, " 6 - agree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 7, "7 - strongly agree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 1, 0, 8, "Prefer not to answer");
+
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 1, "1 - strongly disagree ");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 2, "2- disagree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 3, "3 - somewhat disagree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 4, "4 - neither agree nor disagree ");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 5, "5 - somewhat agree ");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 6, " 6 - agree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 7, "7 - strongly agree");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(1, 2, 0, 8, "Prefer not to answer");
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 1, "Agriculture");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 2, "Bioinformatics and/or computational biology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 3, "Discipline-based education research (DBER)");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 4, "Engineering");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 5, "Computer science and computer engineering");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 6, "Engineering - Biomedical");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 7, "Engineering (other)");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 8, "Environmental science");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 9, "Kinesiology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 10, "anatomy and physiology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 11, "biochemistry");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 12, "general biology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 13, "botany");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 14, "ecology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 15, "evolution");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 16, "genetics");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 17, "zoology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 18, "Marine science");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 19, "Mathematics");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 20, "Neuroscience");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 21, "Physical Sciences");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 22, "Astronomy");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 23, "Chemistry");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 24, "Physics");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 25, "Geology");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 26, "Soil science");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 27, "Statistics and/or Data Science");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 28, "Other");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 1, 29, "prefer not to say");
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 2, 1, "1 student");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 2, 2, "2-4 students");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 2, 3, "5-8 students");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 2, 4, "9-12 students");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 2, 5, "more than 12 students");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 2, 6, "prefer not to say");
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 3, 1, "2");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 3, 2, "3-5");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 3, 3, "6-9");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 3, 4, "10-13");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 3, 5, "More than 13");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 3, 6, "prefer not to say");
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 4, 1, "Faculty member");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 4, 2, "Postdoctoral researcher");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 4, 3, "Graduate student, technician, or staff scientist");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 4, 4, "Other");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 4, 5, "prefer not to say");
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 5, 1, "Laboratory research");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 5, 2, "Field research");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 5, 3, "Computer-based research");
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 5, 4, "prefer not to say");
+
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 6);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 7);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 8);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 9);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 10);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 1, 11);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 2, 1);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 2, 2);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 2, 3);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 2, 4);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 2, 5);
+insert into Possibilities (qtype, survey, QuestionNo, ResponseID, TextPrompt) values(0, 2, 6);
+
+
+-- survey
+insert into Survey (SurveyId, shortName, name, Description) values(1, "URE Experience", "Undergratuate Research Experience Survey", "This survey asks you to share your undergraduate research experience. As a result of you taking this survey, we will try to recommend a variety of possible jobs and careers.");
+insert into Survey (SurveyId, shortName, name, Description) values(2, "Work Experience", "Work Experience Survey", "This survey asks you to share your work experience.");
+
+-- user
+insert into User (UserId, name, Email) values(0, "anonymous","");
+insert into User (UserId, name, Email) values(1, "Morgan Has","MHas366@icloud.com");
+insert into User (UserId, name, Email) values(2, "Oscar Loson","OLoson560@yahoo.com");
+insert into User (UserId, name, Email) values(3, "Sydney Leasure","SLeasure615@hotmail.com");
+insert into User (UserId, name, Email) values(4, "Octavio Talsma","OTalsma32@yahoo.com");
+insert into User (UserId, name, Email) values(5, "Vern Bosley","VBosley84@yahoo.com");
+insert into User (UserId, name, Email) values(6, "Monte Wendlandt","MWendlandt412@msn.com");
+insert into User (UserId, name, Email) values(7, "Bethanie Tankson","BTankson35@icloud.com");
+insert into User (UserId, name, Email) values(8, "Chris Hapeman","CHapeman182@yahoo.com");
+insert into User (UserId, name, Email) values(9, "Andrew Mesia","AMesia23@hotmail.com");
+insert into User (UserId, name, Email) values(10, "Reginald Spiva","RSpiva579@icloud.com");
+insert into User (UserId, name, Email) values(11, "Lajuana Battenhouse","LBattenhouse794@icloud.com");
+insert into User (UserId, name, Email) values(12, "Lin Isreal","LIsreal918@icloud.com");
+insert into User (UserId, name, Email) values(13, "Wilber Gavette","WGavette477@yahoo.com");
+insert into User (UserId, name, Email) values(14, "Andreas Wiechert","AWiechert153@icloud.com");
+insert into User (UserId, name, Email) values(15, "Michel Seabrook","MSeabrook945@icloud.com");
+insert into User (UserId, name, Email) values(16, "Tamisha Melgar","TMelgar570@msn.com");
+insert into User (UserId, name, Email) values(17, "Maurice Dalbeck","MDalbeck773@yahoo.com");
+insert into User (UserId, name, Email) values(18, "Suanne Phaymany","SPhaymany50@yahoo.com");
+insert into User (UserId, name, Email) values(19, "Dean Zimbelman","DZimbelman148@hotmail.com");
+insert into User (UserId, name, Email) values(20, "Salome Lawhead","SLawhead118@gmail.com");
+insert into User (UserId, name, Email) values(21, "Arvilla Mcclurg","AMcclurg955@hotmail.com");
+insert into User (UserId, name, Email) values(22, "Shery Snyder","SSnyder946@yahoo.com");
+insert into User (UserId, name, Email) values(23, "Jong Underkofler","JUnderkofler587@hotmail.com");
+insert into User (UserId, name, Email) values(24, "Michel Aderhold","MAderhold29@yahoo.com");
+insert into User (UserId, name, Email) values(25, "Armando Eichman","AEichman151@msn.com");
+
+-- profile char
+
+-- insert into ProfileChars (Id, dimension, characteristics, descrp) values(, "", "", "");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(1, "Task Characteristics", "Work Scheduling Autonomy", "Work scheduling autonomy means that you are able to determine when, and the order in which, you complete your work tasks without having to check in with a supervisor or coworkers. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(2, "Task Characteristics", "Decision Making Autonomy", "Decision making autonomy means that you are able to make decisions about various aspects of your work without having to check in with a supervisor or coworkers. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(3, "Task Characteristics", "Work Methods Autonomy", "Work methods autonomy means that you are able to choose the approach and methods you use for your work without having to check in with a supervisor or coworker.  ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(4, "Task Characteristics", "Task Variety", "Task variety means that the work you do involves a lot of different kinds tasks and activities, with relatively little time spent doing the same things over and over.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(5, "Task Characteristics", "Task Significance", "Task significance means that the work that you do has – or can have – an important direct or indirect impact on the lives of other people. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(6, "Task Characteristics", "Task Identity", "Task identity means that your work has an identifiable stating point, and ending point, so you know when you’ve ‘finished’ something. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(7, "Task Characteristics", "Feedback from Job", "Feedback from the job means that your work provides you with clear information relating to the quality and quantity of your job performance.  ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(8, "Knowledge Characteristics", "Job Complexity", "Job complexity means that your job requires you to work on multiple, often complicated, tasks simultaneously.  ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(9, "Knowledge Characteristics", "Information Processing", "Information processing means that your job requires you to keep track of and analyze a large amount of information in order to complete your work. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(10, "Knowledge Characteristics", "Problem Solving", "Problem solving means that your job requires you be creative in finding solutions to problems with no conspicuous or obvious answer. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(11, "Knowledge Characteristics", "Skill Variety", "Skill variety means that your job requires you to use a range of different or high-level skills to complete your work tasks. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(12, "Knowledge Characteristics", "Specialization", "Specialization means that your job requires you to have well-developed knowledge, and a focused set of skills, in order to complete your work.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(13, "Social characteristics", "Social Support", "Social support means that your job provides you with the opportunity to develop meaningful personal connections with your coworkers.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(14, "Social characteristics", "Initiated Interdependence ", "Initiated interdependence means that for others to complete their work, including coworkers, depends on you getting your work done first.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(15, "Social characteristics", "Received Interdependence", "Received interdependence means that in order for you to complete your work that others, including coworkers, must complete their work first. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(16, "Social characteristics", "Interaction Outside Research Group", " Interaction outside the research group means that your job requires you to have frequent interactions and communication with people outside of your team or group in order to complete your work.  ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(17, "Social characteristics", "Feedback from Others", " Feedback from others means that, while on the job, coworkers and supervisors provide you with useful information about your work performance.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(18, "Social characteristics", "*Faculty/project leader feedback", "Faculty/project leader feedback means that, while on the job, your faculty/project leader provides you with useful information about your work performance. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(19, "Social characteristics", "*Peer Feedback", " Peer feedback means that, while on the job, your peers provide you with useful information about your work performance.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(20, "Work Context", "Ergonomics", "Ergonomics means that the physical configuration of your work space is generally comfortable, can accommodate a range of different body types, and doesn’t contribute to physical strain or discomfort.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(21, "Work Context", "Physical Demands", "Physical demands means that, while on the job, a great deal of physical effort is necessary to complete your work tasks.   ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(22, "Work Context", "Work Conditions", "Work conditions means that the physical setting where you do your job tasks is generally free from uncomfortable or dangerous conditions.");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(23, "Work Context", "Equipment Use", " Equipment use means that, while on the job, your work requires you to use a number of different kinds of equipment to complete your work tasks. ");
+insert into ProfileChars (Id, dimension, characteristics, descrp) values(24, "Work Context", "**Setting", "Work setting can vary in regards to how time is spent between the laboratory (working with reagents and equipment), computers (working with files), fieldwork (collecting samples or making observations in the natural environment) and working with people (teaching, sales, management work).");
