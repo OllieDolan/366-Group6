cec34f2ea771491ba86dd58ba54699bb7fbda276
Sahra Zhou <tongzhou.chengdu@hotmail.com>
2022-05-24 21:11:54 -0700

    modified create table statements

---
+++ DB-cleanup.sql
@@ -0,0 +1,16 @@
+drop table UREProfiles;
+drop table ProfProfilesVal;
+drop table ProfProfiles;
+drop table DesProfiles;
+drop table OnetProfiles;
+drop table OnetData;
+drop table OnetJobs;
+drop table Responses;
+drop table SurveyResponse;
+drop table charValue;
+drop table Possibilities;
+drop table Questions;
+drop table questionType;
+drop table profileChars;
+drop table Survey;
+drop table User;
--- DDL-setup.sql
+++ DDL-setup.sql
@@ -1,21 +1,21 @@
 create table User(
     UserID int Primary key,
-    name char(40),
-    Email char(64)
+    name varchar(40),
+    Email varchar(64)
 );
 
 -- Profile characteristics table
 create table profileChars(
     Id int Primary key,
-    dimension char(30),
-    characteristics char(30),
-    descrp char(200)
+    dimension varchar(30),
+    characteristics varchar(50) UNIQUE,
+    descrp varchar(200)
 );
 
 create table Survey (
     SurveyID int Primary Key,
-    shortName char(30),
-    name char(60),
+    shortName varchar(30),
+    name varchar(60),
     Description varchar(150)
 );
 create table questionType(
@@ -26,7 +26,7 @@ create table Questions (
     QuestionId int Primary Key,
     question varchar(100),
     qtype int,
-    profChar char(50),
+    profChar varchar(50),
     FOREIGN KEY (Survey) REFERENCES Survey(SurveyID),
     FOREIGN key (qtype) REFERENCES questionType(Id),
     FOREIGN key (profChar) REFERENCES profileChars(characteristics)
@@ -54,11 +54,6 @@ create table charValue(
     FOREIGN KEY (charId) REFERENCES profileChars(Id)
 );
 
-create table UREProfiles(
-    ProfileID int Primary key,
-    Survey int,
-    FOREIGN KEY (Survey) REFERENCES SurveyResponse(ID)
-);
 create table ProfProfiles(
     ProfileID int Primary key
 );
@@ -73,16 +68,15 @@ create table ProfProfilesVal(
 
 create table DesProfiles (
     ProfileID int Primary key,
-    profName char(32),
+    profName varchar(32),
     User int,
-    FOREIGN key (user) REFERENCES Users(UserId)
+    FOREIGN key (user) REFERENCES User(UserId)
 );
 
 create table OnetProfiles (
     ProfileID int Primary key,
     Ranking int,
     Discipline varchar(100)
-    UNIQUE (ProfileID, Ranking)
 );
 
 create table OnetJobs (
@@ -104,7 +98,7 @@ create table SurveyResponse (
     SurvResp int Primary key,
     User int,
     SurveyId int,
-    FOREIGN KEY (User) REFERENCES Users(UserID)
+    FOREIGN KEY (User) REFERENCES User(UserID)
     FOREIGN KEY (SurveyId) REFERENCES Survey(SurveyID)
 );
 
@@ -115,4 +109,9 @@ create table Responses (
     primary key (survResp,QuestionNo),
     FOREIGN KEY (SurvResp) REFERENCES SurveyResponse(SurvResp),
     FOREIGN KEY (QuestionNo) REFERENCES Questions(QuestionId)
+);
+create table UREProfiles(
+    ProfileID int Primary key,
+    Survey int,
+    FOREIGN KEY (Survey) REFERENCES SurveyResponse(SurvResp)
 );
