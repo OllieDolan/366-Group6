ea3b906d442d3576347591eb968813756b2be1d4
Sahra Zhou <tongzhou.chengdu@hotmail.com>
2022-05-25 22:55:04 -0700

    add insert command for all insert statements tested for now

---
+++ DB-insert.sql
@@ -0,0 +1,6 @@
+source SurvUser.sql;
+source ProfCharDesProf.sql;
+source Questions.sql;
+-- possible responses for question type 1 cannot be added
+source Possibilities.sql;
+source UREResponses.sql;
